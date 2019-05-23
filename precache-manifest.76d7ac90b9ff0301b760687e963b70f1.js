ba59f2c4bd616bf139cf5a34e99bff17d0c5e90f
imthug <shrk@sirpi.co>
2019-05-23 08:21:45 +0530

    init react

---
+++ precache-manifest.76d7ac90b9ff0301b760687e963b70f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6356a19864e18a7c8b2ef8a43219ed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "215151de32628329aba0",
+    "url": "/static/css/2.a2f62d9e.chunk.css"
+  },
+  {
+    "revision": "74ce6fd6e0f5375d362b",
+    "url": "/static/css/main.3118060b.chunk.css"
+  },
+  {
+    "revision": "215151de32628329aba0",
+    "url": "/static/js/2.76a49594.chunk.js"
+  },
+  {
+    "revision": "74ce6fd6e0f5375d362b",
+    "url": "/static/js/main.ec0e05a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
