7e352c578459633edb89d36e1e949a184143327f
imthug <shrk@sirpi.co>
2019-05-23 18:17:28 +0530

    build

---
+++ precache-manifest.f4850be8475c20ca95acf3d2062fdda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3606cdb3873941ed0e31fa0181cd21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ef7cfe6041dabd2679",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "c852e0433704c25c8179",
+    "url": "/static/css/main.f95d846b.chunk.css"
+  },
+  {
+    "revision": "f3ef7cfe6041dabd2679",
+    "url": "/static/js/2.48c4a188.chunk.js"
+  },
+  {
+    "revision": "c852e0433704c25c8179",
+    "url": "/static/js/main.0601d50a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
